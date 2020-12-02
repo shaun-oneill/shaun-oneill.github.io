994f2a90e8c6ede3ce5459cffed3f6e186f53d5d
Shaun O'Neill <oshauneill@hotmail.com>
2020-12-01 21:11:06 -0500

    mobile menu updated

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-function navbarOff() {
-  document.getElementById("navbar").style.display = "none";
+let check = false;
+
+function toggleMenu() {
+  if (check == false) {
+    document.getElementById("hamburger-menu").style.position = "static";
+    document.getElementById("hamburger-menu").style.zIndex = 1;
+    check = true;
+  } else {
+    check = false;
+    document.getElementById("hamburger-menu").style.position = "fixed";
+    document.getElementById("hamburger-menu").style.zIndex = -1;
+  }
 }
 
-function navbarOn() {
-  document.getElementById("navbar").style.display = "grid";
-}
+// window.onload = function() {
+//   toggleMenu();
+// }
+
+// function navbarOff() {
+//   document.getElementById("navbar").style.display = "none";
+// }
+
+// function navbarOn() {
+//   document.getElementById("navbar").style.display = "grid";
+// }
